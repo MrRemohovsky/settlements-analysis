f7502c4f7d679039e92d4059a130827b34fce7ca
AA <arshanov8807@mail.ru>
2026-01-11 21:51:13 +0300

    Реализация страницы с детальной информации о муниципалитете

--- settlements/urls.py
+++ settlements/urls.py
@@ -6,5 +6,5 @@ app_name = 'settlements'
 urlpatterns = [
     path('', views.StatsView.as_view(), name='stats'),
     path('regions/<str:region_name>/', views.RegionDetailView.as_view(), name='region'),
-
+    path('regions/<str:region_name>/<str:municipality_name>/', views.MunicipalityDetailView.as_view(), name='municipality'),
 ]
--- settlements/views.py
+++ settlements/views.py
@@ -1,9 +1,9 @@
-from django.db.models import Q
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
-from .services import (
-    TotalStatsService,
-)
+from .models import Municipality
+from .services import TotalStatsService
 from .services.municipality_service import MunicipalityStatsService
 
 
@@ -20,8 +20,6 @@ class StatsView(TemplateView):
 
 
 class RegionDetailView(TemplateView):
-    """Детали конкретного РЕГИОНА"""
-
     template_name = 'settlements/region.html'
 
     def get_context_data(self, **kwargs):
@@ -31,5 +29,53 @@ class RegionDetailView(TemplateView):
         context['region_stats'] = TotalStatsService.get_general_stats(region_name)
         context['population_stats'] = TotalStatsService.get_population_stats_by_region(region_name)
         context['municipalities'] = MunicipalityStatsService.get_municipalities_by_region(region_name)
+        context['region_name'] = region_name
+
+        return context
+
+
+class MunicipalityDetailView(TemplateView):
+    template_name = 'settlements/municipality.html'
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        region_name = self.kwargs['region_name']
+        municipality_name = self.kwargs['municipality_name']
+
+        municipality = get_object_or_404(
+            Municipality,
+            name=municipality_name,
+            region__name=region_name
+        )
+
+        search_query = self.request.GET.get('search', '')
+        settlement_type = self.request.GET.get('type', '')
+        page_number = self.request.GET.get('page', 1)
+
+        context['municipality'] = municipality
+        context['region_name'] = region_name
+        context['population_stats'] = MunicipalityStatsService.get_municipality_population_stats(
+            region_name, municipality_name
+        )
+        context['general_stats'] = MunicipalityStatsService.get_municipality_general_stats(
+            region_name, municipality_name
+        )
+
+        settlements = MunicipalityStatsService.get_municipality_settlements(
+            region_name, municipality_name, search_query, settlement_type
+        )
+
+        paginator = Paginator(settlements, self.paginate_by)
+        page_obj = paginator.get_page(page_number)
+
+        context['page_obj'] = page_obj
+        context['search_query'] = search_query
+        context['selected_type'] = settlement_type
+        context['settlement_types'] = MunicipalityStatsService.get_settlement_types(
+            region_name, municipality_name
+        )
+        context['total_results'] = paginator.count
 
         return context
