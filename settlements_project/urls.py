c1080393dab923c66e63989ab43182d9017844c0
AA <arshanov8807@mail.ru>
2026-01-14 20:04:12 +0300

    Добавлен редирект и убран отступ у шапки

--- settlements_project/urls.py
+++ settlements_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('settlements/', include('settlements.urls')),
+    path('', RedirectView.as_view(url='/settlements/')),
 ]
